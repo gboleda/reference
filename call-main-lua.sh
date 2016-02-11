cc5cd31bf8b58b4cf96597551b6031df7b92d0db
gboleda <gemma.boleda@upf.edu>
2016-02-11 22:09:57 +0100

    Revert "polished a bit"
    This reverts commit 2eabf7173bbcf9f26e8d0efdd7d5b8b5756e5d80.

---
+++ call-main-lua.sh
@@ -0,0 +1,2 @@
+th -i main.lua --word_lexicon_file word-lexicon-toy.dm --image_dataset_file image-dataset-toy.dm --stimuli_prefix stimuli
+
--- data.lua
+++ data.lua
@@ -205,3 +205,19 @@ function create_input_structures_from_file(i_file,data_set_size,t_input_size,v_i
 end
 
 -- REAL DATA TO HERE
+
+--[===[
+OLD
+function load_data(word_lexicon_file,image_dataset_file,protocol_prefix)
+   --
+   print("Hi there!")
+--   print(word_lexicon_file)
+   -- -- tensor of size 
+   -- local training_word_query_list=torch.randn(training_set_size,t_input_size)
+   -- local validation_word_query_list=torch.randn(validation_set_size,t_input_size)
+   
+   -- return training_word_query_list,validation_word_query_list,training_image_set_list,validation_image_set_list,training_index_list,validation_index_list,training_set_size,validation_set_size,image_set_size,t_input_size,v_input_size
+      return true
+end
+--]===]
+
