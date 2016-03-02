8155d66d6097f2544ac63bf5fcbd1f2c93c4e127
Marco Baroni <marco.baroni@unitn.it>
2016-03-02 16:49:54 +0100

    updated paths in convnet script

--- output_convnet_guesses.m
+++ output_convnet_guesses.m
@@ -19,12 +19,14 @@ run /Users/marco/Desktop/matconvnet-1.0-beta18/matlab/vl_setupnn
 net = load('/Users/marco/Desktop/matconvnet-1.0-beta18/imagenet-vgg-verydeep-19.mat') ;
 
 % load images
-imagefiles = dir('/Users/marco/Desktop/matconvnet-1.0-beta18/*.jpg');
+%imagefiles = dir('/Users/marco/Desktop/matconvnet-1.0-beta18/*.jpg');
+imagefiles = dir('/Users/marco/Desktop/trial/*.jpg');
 
 out_file = fopen('convnet_guesses.txt','w');
 
 for im_index=1:length(imagefiles)
-  im = imread(fullfile('/Users/marco/Desktop/matconvnet-1.0-beta18',imagefiles(im_index).name)) ;
+%  im = imread(fullfile('/Users/marco/Desktop/matconvnet-1.0-beta18',imagefiles(im_index).name)) ;
+  im = imread(fullfile('/Users/marco/Desktop/trial',imagefiles(im_index).name)) ;
   im_ = single(im) ; % note: 0-255 range
   im_ = imresize(im_, net.meta.normalization.imageSize(1:2)) ;
   im_=im_-repmat(net.meta.normalization.averageImage,224);
