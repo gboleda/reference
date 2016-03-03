6debec5726133641d91ac2ca06f547f20d73efc2
Marco Baroni <marco.baroni@unitn.it>
2016-03-03 23:13:44 +0100

    cleanup

--- cnn_baseline/output_convnet_guesses.m
+++ cnn_baseline/output_convnet_guesses.m
@@ -18,14 +18,12 @@ run /Users/marco/Desktop/matconvnet-1.0-beta18/matlab/vl_setupnn
 net = load('/Users/marco/Desktop/matconvnet-1.0-beta18/imagenet-vgg-verydeep-19.mat') ;
 
 % load images
-%imagefiles = dir('/Users/marco/Desktop/temp_images/*.jpg');
-imagefiles = dir('/Users/marco/Desktop/still_to_process/*.jpg');
+imagefiles = dir('/Users/marco/Desktop/temp_images/*.jpg');
 
 out_file = fopen('convnet_guesses.txt','w');
 
 for im_index=1:length(imagefiles)
-%  im = imread(fullfile('/Users/marco/Desktop/temp_images',imagefiles(im_index).name));
-im = imread(fullfile('/Users/marco/Desktop/still_to_process',imagefiles(im_index).name));
+  im = imread(fullfile('/Users/marco/Desktop/temp_images',imagefiles(im_index).name));
 
   im_ = single(im) ; % note: 0-255 range
   im_ = imresize(im_, net.meta.normalization.imageSize(1:2)) ;
