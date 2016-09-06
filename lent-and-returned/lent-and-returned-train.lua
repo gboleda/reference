3ae7bcac46f3d6f34ba90f80222a01d7a95f4697
Marco Baroni <marco.baroni@unitn.it>
2016-09-06 17:02:55 +0200

    ongoing bug fixing

--- lent-and-returned/lent-and-returned-train.lua
+++ lent-and-returned/lent-and-returned-train.lua
@@ -277,6 +277,9 @@ function test(input_table,gold_index_list)
    -- type
    local hit_count=0
    if (opt.use_cuda~=0) then
+      --debug
+      print(gold_index_list:type())
+      print(model_guesses_indices:type())
       hit_count=torch.sum(torch.eq(gold_index_list,model_guesses_indices))
    else
       hit_count=torch.sum(torch.eq(gold_index_list:long(),model_guesses_indices))
