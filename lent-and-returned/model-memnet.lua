928cf52611a3c74ac9cc3479ce2458458a01aa27
gboleda <gemma.boleda@upf.edu>
2016-11-21 15:01:20 +0100

    small bug memnn

--- lent-and-returned/model-memnet.lua
+++ lent-and-returned/model-memnet.lua
@@ -1,6 +1,6 @@
 -- our reimplementation of memory networks, with variants
 
-function soft_retrieval_standard(query,entity_matrix,output_entity_matrix,temperature)
+function soft_retrieval_memn(query,entity_matrix,output_entity_matrix,temperature)
    -- we take the dot product of each row vector in the
    -- memory with the linguistic query vector, to obtain a
    -- memory-to-query similarity profile, that we softmax normalize
@@ -13,12 +13,18 @@ function soft_retrieval_standard(query,entity_matrix,output_entity_matrix,temper
    local query_entity_similarity_profile = nn.View(1,-1):setNumInputDims(1)(nn.SoftMax()(rescaled_query_entity_similarity_profile)):annotate{name='query_entity_similarity_profile'}
       
    -- we now do "soft retrieval" of the memory vector(s) that match
-   -- the query: we obtain a vector that is a weighted sum of all
-   -- the memory vectors in the *output* memory (weights =
-   -- similarity profile, such that we will return the most similar
-   -- vector(s) to the query) (we get a matrix of such vectors
-   -- because of mini-batches)
-   local retrieved_entity_vector = nn.View(-1):setNumInputDims(2)(nn.MM(false,false)({query_entity_similarity_profile,output_entity_matrix}))
+   -- the query: we obtain a vector that is a weighted sum of all the
+   -- memory vectors in the memory (weights = similarity profile, such
+   -- that we will return the most similar vector(s) to the query) (we
+   -- get a matrix of such vectors because of mini-batches) (the output
+   -- is a minibatch x 1 x multi_modal_size matrix, to match with the
+   -- rest of the code)
+   local retrieved_entity_vector = nil
+   if output_entity_matrix == nil then -- in this case it's the one matrix version, or our model
+      retrieved_entity_vector = nn.View(-1):setNumInputDims(1)(nn.MM(false,false)({query_entity_similarity_profile,entity_matrix}))
+   else -- this is the standard MemNN; the soft retrieval is on the *output* memory matrix
+      retrieved_entity_vector = nn.View(-1):setNumInputDims(1)(nn.MM(false,false)({query_entity_similarity_profile,output_entity_matrix}))
+   end
    return retrieved_entity_vector
 end
 
@@ -124,7 +130,7 @@ function mm_standard(t_inp_size,v_inp_size,mm_size,inp_seq_cardinality,candidate
    -- representation with the retrieved vector at each hop
 
    -- this is a 1-dimensional vector, so it can be added to the query
-   local retrieved_entity_vector = soft_retrieval_standard(query,entity_matrix,output_entity_matrix,temperature)
+   local retrieved_entity_vector = soft_retrieval_memn(query,entity_matrix,output_entity_matrix,temperature)
    if hops_count == 1 then
       query = nn.CAddTable()({query,retrieved_entity_vector})
    else
@@ -252,52 +258,35 @@ function mm_one_matrix(t_inp_size,v_inp_size,mm_size,inp_seq_cardinality,candida
    table.insert(shareList,token_object_mappings)
 
    -- now we perform retrieval multiple times, updating the query
-   -- representation with the retrieved vector at each hope
-   local retrieved_entity_matrix = nil -- initialized here so it's in the main scope
-   local query_to_query_mappings = {} -- to share parameters of linear
-				      -- transformation of query from
-				      -- one hop to the other
-   
-   for current_hop_stage=1,hops_count do
-      if current_hop_stage > 1 then
+   -- representation with the retrieved vector at each hop
+
+   -- this is a 1-dimensional vector, so it can be added to the query
+   local retrieved_entity_vector = soft_retrieval_memn(query,entity_matrix,nil,temperature)
+   if hops_count == 1 then
+      query = nn.CAddTable()({query,retrieved_entity_vector})
+   else
+      local query_to_query_mappings = {} -- to share parameters of linear
+      -- transformation of query from
+      -- one hop to the other
+      for current_hop_stage=1,hops_count do
 	 -- update query by summing current retrieved vector to it, after a linear mapping of the current query
 	 local mapped_query = nn.LinearNB(mm_size, mm_size)(query)
-	 table.insert(query_to_query_mappings,mapped_query)       -- parameters of this mapping are shared across hopes
-	 query = nn.CAddTable()({mapped_query,retrieved_entity_matrix})
-      end
-      
-      -- we take the dot product of each row (entity) vector in the
-      -- memory with the linguistic query vector, to obtain a
-      -- memory-to-query similarity profile, that we softmax normalize
-      -- (note Views needed to get right shapes, and rescaling by
-      -- temperature)
-      -- query needs to be a column vector
-      local query_as_column = nn.View(-1,1):setNumInputDims(1)(query)
-      local raw_query_entity_similarity_profile = nn.View(-1):setNumInputDims(2)(nn.MM(false,false)({entity_matrix,query_as_column}))
-      local rescaled_query_entity_similarity_profile = nn.MulConstant(temperature)(raw_query_entity_similarity_profile)
-      local query_entity_similarity_profile = nn.View(1,-1):setNumInputDims(1)(nn.SoftMax()(rescaled_query_entity_similarity_profile)):annotate{name='query_entity_similarity_profile'}
-      
-      -- we now do "soft retrieval" of the output matrix vector that matches the query:
-      -- we obtain a vector that is a weighted sum of all the memory
-      -- vectors in the output memory matrix (weights = similarity profile, such
-      -- that we will return the entity that is most similar to the
-      -- query) (we get a matrix of such vectors because of mini-batches)
-      retrieved_entity_matrix = nn.MM(false,false)({query_entity_similarity_profile,entity_matrix})
-
-   end -- end of hopping
-
-   -- adding query to query mapping parameters to table of parameter sets sharing weights
-   if #query_to_query_mappings > 0 then -- only if there is more than one hop
+	 table.insert(query_to_query_mappings,mapped_query)       -- parameters of this mapping are shared across hops
+	 query = nn.CAddTable()({mapped_query,retrieved_entity_vector})
+      end -- end of hopping
+      -- adding query to query mapping parameters to table of parameter sets sharing weights
       table.insert(shareList,query_to_query_mappings)
    end
 
-   -- the final prediction is the updated query passed through a
-   -- mapping
-   prediction = nn.LinearNB(mm_size, mm_size)(query)
-   
+   -- to generate the final prediction vector, we pass the last
+   -- version of the query through a linear mapping
+   local final_prediction = nn.LinearNB(mm_size,mm_size)(query)
+   -- reshaping final_prediction so it is of the right form to match the function return_entity_image
+   final_prediction_column = nn.View(-1,1,mm_size):setNumInputDims(2)(final_prediction)
+
    -- now we call the return_entity_image function to obtain a softmax
    -- over candidate images
-   local output_distribution=return_entity_image(v_inp_size,mm_size,candidate_cardinality,dropout_p,inputs,shareList,prediction)
+   local output_distribution=return_entity_image(v_inp_size,mm_size,candidate_cardinality,dropout_p,inputs,shareList,final_prediction_column)
    
    -- wrapping up the model
    local model = nn.gModule(inputs,{output_distribution})
