dbf46edecbc80acd75098f75dad0559209096d6c
thenghiapham <phamthenghia1985@gmail.com>
2017-01-08 20:23:13 +0100

    modify model with 2 matrices

--- lent-and-returned/model_matrix.lua
+++ lent-and-returned/model_matrix.lua
@@ -35,13 +35,13 @@ function entity_prediction_image_att_shared_neprob_with_2_matrices(t_inp_size,v_
    -- a table to store tables of connections that must share parameters
    local shareList = {}
 
-   -- table to collect all attribute mappings, to be shared
-   local attribute_mappings = {}
+   local attribute_mappings_select= {}
+   local attribute_mappings_compare = {}
 
    -- first, we process the query, mapping it onto multimodal space
    -- the query attributes in the query
-   local query_attribute_1 = add_new_input_and_create_mapping(inputs,t_inp_size,mm_size,dropout_p,attribute_mappings)
-   local query_attribute_2 = add_new_input_and_create_mapping(inputs,t_inp_size,mm_size,dropout_p,attribute_mappings)
+   local query_attribute_1 = add_new_input_and_create_mapping(inputs,t_inp_size,mm_size,dropout_p,attribute_mappings_compare)
+   local query_attribute_2 = add_new_input_and_create_mapping(inputs,t_inp_size,mm_size,dropout_p,attribute_mappings_compare)
    -- the object name in the query
    -- no shared mapping (already a flexibility)
    local query_object = add_new_input_and_create_mapping(inputs,t_inp_size,mm_size,dropout_p,{})
@@ -53,42 +53,47 @@ function entity_prediction_image_att_shared_neprob_with_2_matrices(t_inp_size,v_
 
    -- now we process the object tokens
    -- a table to store the entity matrix as it evolves through time
-   local entity_matrix_table = {}
+   local entity_matrix_table_select = {}
+   local entity_matrix_table_compare = {}
    -- tables where to store the connections that must share weights:
    ---- token object mappings
-   local token_object_mappings = {}
+   local token_object_mappings_select = {}
+   local token_object_mappings_compare = {}
    --- mappings to raw new entity mass
    local raw_new_entity_mass_mappings = {}
 
    -- the first object token is a special case, as it will always be
    -- directly mapped to the first row of the entity matrix
    
-   local first_object_token_vector = add_and_compute_token_vector(inputs,t_inp_size,v_inp_size,mm_size,dropout_p,attribute_mappings,token_object_mappings)
+   local first_object_token_vector_select = add_and_compute_token_vector(inputs,t_inp_size,v_inp_size,mm_size,dropout_p,attribute_mappings_select,token_object_mappings_select)
+   local first_object_token_vector_compare = add_and_compute_token_vector(inputs,t_inp_size,v_inp_size,mm_size,dropout_p,attribute_mappings_compare,token_object_mappings_compare)
    -- turning the vector into a 1xmm_size matrix, adding the latter as
    -- first state of the entity matrix table
-   table.insert(entity_matrix_table,nn.View(1,-1):setNumInputDims(1)(first_object_token_vector))
+   table.insert(entity_matrix_table_select,nn.View(1,-1):setNumInputDims(1)(first_object_token_vector_select))
+   table.insert(entity_matrix_table_compare,nn.View(1,-1):setNumInputDims(1)(first_object_token_vector_compare))
 
    -- now we process all the other object tokens in a loop
    for i=2,inp_seq_cardinality do
       
-      local object_token_vector_flat = add_and_compute_token_vector(inputs,t_inp_size,v_inp_size,mm_size,dropout_p,attribute_mappings,token_object_mappings):annotate{name='object_token_' .. i}
+      local object_token_vector_flat_select = add_and_compute_token_vector(inputs,t_inp_size,v_inp_size,mm_size,dropout_p,attribute_mappings_select,token_object_mappings_select):annotate{name='object_token_' .. i}
+      local object_token_vector_flat_compare = add_and_compute_token_vector(inputs,t_inp_size,v_inp_size,mm_size,dropout_p,attribute_mappings_compare,token_object_mappings_compare):annotate{name='object_token_' .. i}
       -- reshaping
-      local object_token_vector = nn.View(-1,1):setNumInputDims(1)(object_token_vector_flat)
+      local object_token_vector_select = nn.View(-1,1):setNumInputDims(1)(object_token_vector_flat_select)
+      local object_token_vector_compare = nn.View(-1,1):setNumInputDims(1)(object_token_vector_flat_compare)
 
       -- measuring the similarity of the current vector to the ones in
       -- the previous state of the entity matrix
-      local raw_similarity_profile_to_entity_matrix = nn.MM(false,false)
-      ({entity_matrix_table[i-1],object_token_vector})
+      local raw_similarity_profile_to_entity_matrix_select = nn.MM(false,false)({entity_matrix_table_select[i-1],object_token_vector_select})
 
       -- computing the new-entity cell value
       -- average or max or sum by default of input vector cells...
       local raw_cumulative_similarity=nil
       if (opt.new_mass_aggregation_method=='mean') then
-        raw_cumulative_similarity=nn.Mean(1,2)(raw_similarity_profile_to_entity_matrix)
+        raw_cumulative_similarity=nn.Mean(1,2)(raw_similarity_profile_to_entity_matrix_select)
       elseif (opt.new_mass_aggregation_method=='max') then
-         raw_cumulative_similarity=nn.Max(1,2)(raw_similarity_profile_to_entity_matrix)
+         raw_cumulative_similarity=nn.Max(1,2)(raw_similarity_profile_to_entity_matrix_select)
       else -- sum by default
-         raw_cumulative_similarity = nn.Sum(1,2)(raw_similarity_profile_to_entity_matrix)
+         raw_cumulative_similarity = nn.Sum(1,2)(raw_similarity_profile_to_entity_matrix_select)
       end
       raw_cumulative_similarity:annotate{name='raw_cumulative_similarity_' .. i}
       local raw_new_entity_mass = nn.Linear(1,1)(raw_cumulative_similarity):annotate{name='raw_new_entity_mass_' .. i}
@@ -105,7 +110,6 @@ function entity_prediction_image_att_shared_neprob_with_2_matrices(t_inp_size,v_
           elseif (nonlinearity == 'tanh') then
              transformed_new_entity_mass = nn.Tanh()(raw_new_entity_mass)
           else -- sigmoid is leftover option: if (nonlinearity == 'sigmoid') then
-             print("Really using sigmoid")
              transformed_new_entity_mass = nn.Sigmoid()(raw_new_entity_mass)
           end
       end
@@ -117,7 +121,7 @@ function entity_prediction_image_att_shared_neprob_with_2_matrices(t_inp_size,v_
       -- a ROW vector
       --transformed_new_entity_mass = nn.Peek("create new", true)(transformed_new_entity_mass)
       local minus_transform_new_entity_mass = nn.AddConstant(1,false)(nn.MulConstant(-1,false)(transformed_new_entity_mass))
-      local normalized_similarity_profile = nn.SoftMax()(nn.View(-1):setNumInputDims(2)(raw_similarity_profile_to_entity_matrix)):annotate{name='normalized_similarity_profile_' .. i}
+      local normalized_similarity_profile = nn.SoftMax()(nn.View(-1):setNumInputDims(2)(raw_similarity_profile_to_entity_matrix_select)):annotate{name='normalized_similarity_profile_' .. i}
       --normalized_similarity_profile = nn.Peek("softmax", true)(normalized_similarity_profile)
       normalized_similarity_profile = nn.MM(false, false){nn.View(-1,i - 1, 1)(normalized_similarity_profile),nn.View(-1,1, 1)(minus_transform_new_entity_mass)}
       normalized_similarity_profile = (nn.JoinTable(2,2)({nn.View(-1,i - 1)(normalized_similarity_profile),transformed_new_entity_mass}))
@@ -126,20 +130,24 @@ function entity_prediction_image_att_shared_neprob_with_2_matrices(t_inp_size,v_
       -- token vector, multiplied by the corresponding entry on the
       -- normalized similarity profile (including, in the final row,
       -- weighting by the normalized new mass cell): 
-      local weighted_object_token_vector_matrix = nn.MM(false,true){nn.View(-1,1):setNumInputDims(1)(normalized_similarity_profile),object_token_vector}
+      local weighted_object_token_vector_matrix_select = nn.MM(false,true){nn.View(-1,1):setNumInputDims(1)(normalized_similarity_profile),object_token_vector_select}
+      local weighted_object_token_vector_matrix_compare = nn.MM(false,true){nn.View(-1,1):setNumInputDims(1)(normalized_similarity_profile),object_token_vector_compare}
 
       -- at this point we update the entity matrix by adding the
       -- weighted versions of the current object token vector to each
       -- row of it (we pad the bottom of the entity matrix with a zero
       -- row, so that we can add it to the version of the current
       -- object token vector that was weighted by the new mass cell
-      entity_matrix_table[i]= nn.CAddTable(){
-         nn.Padding(1,1,2)(entity_matrix_table[i-1]),weighted_object_token_vector_matrix}:annotate{'entity_matrix_table' .. i}
+      entity_matrix_table_select[i]= nn.CAddTable(){
+         nn.Padding(1,1,2)(entity_matrix_table_select[i-1]),weighted_object_token_vector_matrix_select}:annotate{'entity_matrix_table' .. i}
+         entity_matrix_table_compare[i]= nn.CAddTable(){
+         nn.Padding(1,1,2)(entity_matrix_table_compare[i-1]),weighted_object_token_vector_matrix_compare}:annotate{'entity_matrix_table' .. i}
    end
    -- end of processing input objects
 
    -- adding to shareList here the attribute and entity mass mappings
-   table.insert(shareList,attribute_mappings)
+   table.insert(shareList,attribute_mappings_select)
+   table.insert(shareList,attribute_mappings_compare)
    table.insert(shareList,raw_new_entity_mass_mappings)
 
 
@@ -148,7 +156,7 @@ function entity_prediction_image_att_shared_neprob_with_2_matrices(t_inp_size,v_
    -- obtain an entity-to-query similarity profile, that we softmax
    -- normalize (note Views needed to get right shapes, and rescaling
    -- by temperature)
-   local raw_query_entity_similarity_profile = nn.View(-1):setNumInputDims(2)(nn.MM(false,false)({entity_matrix_table[inp_seq_cardinality],query}))
+   local raw_query_entity_similarity_profile = nn.View(-1):setNumInputDims(2)(nn.MM(false,false)({entity_matrix_table_compare[inp_seq_cardinality],query}))
    local rescaled_query_entity_similarity_profile = nn.MulConstant(temperature)(raw_query_entity_similarity_profile)
    local query_entity_similarity_profile = nn.View(1,-1):setNumInputDims(1)(nn.SoftMax()(rescaled_query_entity_similarity_profile)):annotate{name='query_entity_similarity_profile'}
 
@@ -157,14 +165,15 @@ function entity_prediction_image_att_shared_neprob_with_2_matrices(t_inp_size,v_
    -- vectors in the entity library (weights= similarity profile, such
    -- that we will return the entity that is most similar to the
    -- query) (we get a matrix of such vectors because of mini-batches)
-   local retrieved_entity_matrix = nn.MM(false,false)({query_entity_similarity_profile,entity_matrix_table[inp_seq_cardinality]})
+   local retrieved_entity_matrix = nn.MM(false,false)({query_entity_similarity_profile,entity_matrix_table_compare[inp_seq_cardinality]})
    
    -- now we call the return_entity_image_shared function to obtain a softmax
    -- over candidate images
-   local output_distribution=return_entity_image_shared(v_inp_size,mm_size,candidate_cardinality,dropout_p,inputs,token_object_mappings,retrieved_entity_matrix)
+   local output_distribution=return_entity_image_shared(v_inp_size,mm_size,candidate_cardinality,dropout_p,inputs,token_object_mappings_compare,retrieved_entity_matrix)
    
    -- adding token_object_mappings to shareList only now, after we also added to it the candidate image mappings
-   table.insert(shareList,token_object_mappings)
+   table.insert(shareList,token_object_mappings_select)
+   table.insert(shareList,token_object_mappings_compare)
 
 
    -- wrapping up the model
