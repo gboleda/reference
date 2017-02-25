b0314c87a7166f70315e6da49a77b62c8acc6931
thenghiapham <phamthenghia1985@gmail.com>
2017-02-25 12:12:51 +0100

    move old code to separte folder

--- Peek.lua
+++ Peek.lua
@@ -12,27 +12,42 @@ g:forward(torch.rand(2))
 --]]
 
 local Peek, parent = torch.class('nn.Peek', 'nn.Module')
-function Peek:__init(report_func)
-   parent.__init(self)
-   self.report_func = report_func
+
+function Peek:__init(name, print_frequency, print_type)
+  self.name = name
+  self.print_type = print_type
+  self.print_frequency = print_frequency
+  self.iteration = -1
 end
- 
+
 function Peek:updateOutput(input)
-    print('Module input')
-    
-    self.output=input
-    if self.report_func then
-      print(self.report_func(input))
-    else
-      print(input)
+  self.iteration = self.iteration + 1
+  if (self.iteration % self.print_frequency == 0) then
+    print(self.name)
+    local sizeString = input:size(1)
+    local num_e = input:size(1)
+    if input:dim() > 1 then
+      sizeString = sizeString .. "," input:size(2)
+      num_e = num_e * input:size(2)
     end
-      
-    return self.output
+    if input:dim() > 2 then
+      sizeString = sizeString .. "," input:size(3)
+      num_e = num_e * input:size(3)
+    end
+    if (self.print_type == 1) then -- print size
+      print(sizeString)
+    elseif (self.print_type == 2) then--print matrix
+      print(input)
+    else -- average abs
+      print(input:norm() / math.sqrt(num_e))      
+    end
+  end
+  return input
 end
+
 function Peek:updateGradInput(input, gradOutput)
     print('Module grad output')
     print(gradOutput)
     self.gradInput = gradOutput
     return self.gradInput
 end
-
